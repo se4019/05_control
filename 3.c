4758e361e8db7b37ea100fc6e1db8a40d8c62495
se4019 <70657061+se4019@users.noreply.github.com>
2020-10-06 16:13:59 +0900

    03

---
+++ 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int num=0;
+	char c;
+	
+	printf("Input a string:");
+	
+	while( (c=getchar()) != '/n')
+	{
+		if ( c >= '0' && c<= '9')	
+		{
+			num++;
+		}
+	}
+	
+	printf("the number of digits is %i/n", num);
+	
+	return 0;
+}
